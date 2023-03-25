e2ba3ff72784a677ab0c3ecb21e5f605d7989f63
Afaan Bilal <afaanbilal@gmail.com>
2023-03-25 20:01:50 +0530

    ✨ Rocket: Request and Response Data

--- src/controllers/auth.rs
+++ src/controllers/auth.rs
@@ -6,11 +6,30 @@
  * @link   https://github.com/AfaanBilal/bookstore
  */
 use super::Response;
-use rocket::State;
+use rocket::{
+    serde::{json::Json, Deserialize, Serialize},
+    State,
+};
 use sea_orm::DatabaseConnection;
 
-#[post("/sign-in")]
-pub async fn sign_in(db: &State<DatabaseConnection>) -> Response<String> {
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct ReqSignIn {
+    email: String,
+    password: String,
+}
+
+#[derive(Serialize, Deserialize, Responder)]
+#[serde(crate = "rocket::serde")]
+pub struct ResSignIn {
+    token: String,
+}
+
+#[post("/sign-in", data = "<req_sign_in>")]
+pub async fn sign_in(
+    db: &State<DatabaseConnection>,
+    req_sign_in: Json<ReqSignIn>,
+) -> Response<ResSignIn> {
     let db = db as &DatabaseConnection;
     todo!()
 }

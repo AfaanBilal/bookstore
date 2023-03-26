235df52a4b9f8e1bb844e858a5a55296e523774a
Afaan Bilal <afaanbilal@gmail.com>
2023-03-26 11:08:09 +0530

    ✨ Rocket: Request and Response Data for Sign Up

--- src/controllers/auth.rs
+++ src/controllers/auth.rs
@@ -34,7 +34,20 @@ pub async fn sign_in(
     todo!()
 }
 
-#[post("/sign-up")]
-pub async fn sign_up() -> Response<String> {
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct ReqSignUp {
+    email: String,
+    password: String,
+    firstname: Option<String>,
+    lastname: Option<String>,
+}
+
+#[post("/sign-up", data = "<req_sign_up>")]
+pub async fn sign_up(
+    db: &State<DatabaseConnection>,
+    req_sign_up: Json<ReqSignUp>,
+) -> Response<String> {
+    let db = db as &DatabaseConnection;
     todo!()
 }

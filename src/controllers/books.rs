599b47a5a11402f720886efb4bafa50623b09dce
Afaan Bilal <afaanbilal@gmail.com>
2023-03-27 16:55:38 +0530

    ✨ Book: List

--- src/controllers/books.rs
+++ src/controllers/books.rs
@@ -5,11 +5,62 @@
  * @link   https://afaan.dev
  * @link   https://github.com/AfaanBilal/bookstore
  */
-use super::Response;
+use rocket::{
+    http::Status,
+    serde::{json::Json, Serialize},
+    State,
+};
+use sea_orm::*;
+
+use super::{Response, SuccessResponse};
+use crate::auth::AuthenticatedUser;
+use crate::entities::{book, prelude::*};
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct ResBook {
+    id: i32,
+    author_id: i32,
+    title: String,
+    year: String,
+    cover: String,
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct ResBookList {
+    total: usize,
+    books: Vec<ResBook>,
+}
 
 #[get("/")]
-pub async fn index() -> Response<String> {
-    todo!()
+pub async fn index(
+    db: &State<DatabaseConnection>,
+    _user: AuthenticatedUser,
+) -> Response<Json<ResBookList>> {
+    let db = db as &DatabaseConnection;
+
+    let books = Book::find()
+        .order_by_desc(book::Column::UpdatedAt)
+        .all(db)
+        .await?
+        .iter()
+        .map(|b| ResBook {
+            id: b.id,
+            author_id: b.author_id,
+            title: b.title.to_owned(),
+            year: b.year.to_owned(),
+            cover: b.cover.to_owned(),
+        })
+        .collect::<Vec<_>>();
+
+    Ok(SuccessResponse((
+        Status::Ok,
+        Json(ResBookList {
+            total: books.len(),
+            books,
+        }),
+    )))
 }
 
 #[post("/")]
